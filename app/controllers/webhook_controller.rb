c4725324f0e70d18cc3e1431cb230c2e47d0beab
tatsuki_tokumine <tatsuki_tokumine@tatsuki-tokuminenoMacBook-Pro.local>
2020-03-24 12:00:08 +0900

    捨て

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -37,31 +37,11 @@ class WebhookController < ApplicationController
               text: 'サンホセ'
             }
           elsif event.message['text'].eql?('JP') then
-            # 取得先URL
-            url_holiday = 'https://date.nager.at/Api/v1/Get/'
-
-            params = {
-              countrycode = 'JP'
-              year = '2020'
-            }
-
-            client = HTTPClient.new
-            request = client.get(url_holiday, params)
-            response = JSON.parse(require.body)
-
-            # 祝日保管用Array
-            holiday_list = []
-            # 応答メッセージ
-            res_holiday = {}
-
-            response.each do |res|
-              holiday_list.append res["name"]
-              res_holiday += res["name"]
-              res_holiday += "¥n"
-            end
+            # JPと入力したら日本の2020年の祝日リストを返す
+            get_holidays(event.message['text'], '2020')
             message = {
               type: 'text',
-              text: res_holiday
+              text: holidays_list
             }
           else
             # Nager.Dateをコール
@@ -81,5 +61,40 @@ class WebhookController < ApplicationController
       end
     }
     head :ok
+  end
+
+  def get_holidays(countrycode, year, retry_count = 10)
+    raise ArgumentError, 'too many HTTP redirects' if retry_count == 0
+
+    uri = Addressable::URI.parse("https://date.nager.at/Api/v1/Get/#{countrycode}/#{year}")
+
+    begin
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.open_timeout = 5
+        http.read_timeout = 10
+        http.get(uri.request_uri)
+      end
+
+      case response
+        when Net::HTTPSuccess
+          json = JSON.parse(response.body)
+          if json['results_returned'] == 0
+            nil
+          else
+            json
+          end
+
+        when Net::HTTPRedirection
+          location = response['location']
+          Rails.logger.error(warn "redirected to #{location}")
+          search_area(form_words, start_date, end_date, retry_count - 1)
+        else
+          Rails.logger.error([uri.to_s, response.value].join(" : "))
+      end
+
+    rescue => e
+      Rails.logger.error(e.message)
+      raise e
+    end
   end
 end
