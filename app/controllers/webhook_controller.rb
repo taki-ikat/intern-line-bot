094aac01bf574c7e8a5dc9547c4d645bc478e0dd
tatsuki_tokumine <tatsuki_tokumine@tatsuki-tokuminenoMacBook-Pro.local>
2020-03-24 14:14:31 +0900

    レベル1

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -36,17 +36,7 @@ class WebhookController < ApplicationController
               type: 'text',
               text: 'サンホセ'
             }
-          elsif event.message['text'].eql?('JP') then
-            # JPと入力したら日本の2020年の祝日リストを返す
-            get_holidays(event.message['text'], '2020')
-            message = {
-              type: 'text',
-              text: holidays_list
-            }
           else
-            # Nager.Dateをコール
-            # api/v2/AvailableCountriesで辞書型リストゲット
-            # contrycodes={'code': 'name'}
             message = {
               type: 'text',
               text: event.message['text']
@@ -61,40 +51,5 @@ class WebhookController < ApplicationController
       end
     }
     head :ok
-  end
-
-  def get_holidays(countrycode, year, retry_count = 10)
-    raise ArgumentError, 'too many HTTP redirects' if retry_count == 0
-
-    uri = Addressable::URI.parse("https://date.nager.at/Api/v1/Get/#{countrycode}/#{year}")
-
-    begin
-      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.open_timeout = 5
-        http.read_timeout = 10
-        http.get(uri.request_uri)
-      end
-
-      case response
-        when Net::HTTPSuccess
-          json = JSON.parse(response.body)
-          if json['results_returned'] == 0
-            nil
-          else
-            json
-          end
-
-        when Net::HTTPRedirection
-          location = response['location']
-          Rails.logger.error(warn "redirected to #{location}")
-          search_area(form_words, start_date, end_date, retry_count - 1)
-        else
-          Rails.logger.error([uri.to_s, response.value].join(" : "))
-      end
-
-    rescue => e
-      Rails.logger.error(e.message)
-      raise e
-    end
   end
 end
